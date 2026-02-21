25bad6785552e570a381e6d74a9c8758dabbbf52
shirishp16 <shirishparasa@gmail.com>
2026-02-21 13:53:32 -0500

    Config + README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # LingoTax
+
+Tech Stack:
+![LingoTax Tech Stack:](/techstack.jpeg)
+
+Flow Chart:
+
+![LingoTax Flow Chart:](/flowchart.jpeg)
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -9,7 +8,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "lh3.googleusercontent.com", // Google profile photos
+        hostname: "lh3.googleusercontent.com",
       },
     ],
   },
